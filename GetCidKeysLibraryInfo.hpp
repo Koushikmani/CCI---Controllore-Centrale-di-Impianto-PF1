cbf6913c97b8a7cf4ca71e807301db24beea0353
Koushikmani <81651764+Koushikmani@users.noreply.github.com>
2026-04-02 11:10:03 +0200

    Add files via upload

---
+++ GetCidKeysLibraryInfo.hpp
@@ -0,0 +1,8 @@
+#include "Arp/Base/Core/Arp.hpp"
+
+namespace GetCidKeys
+{
+
+	const ArpVersion GetCidKeysLibraryVersion = ArpVersion(1); // adjust your library version number here
+
+} // end of namespace GetCidKeys
